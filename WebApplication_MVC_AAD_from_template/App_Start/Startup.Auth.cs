dc54f40c62ade676c179b123ace202a2b8ef004f
mddazure <mddazure@gmail.com>
2019-12-24 12:24:54 +0100

    webapp with add behind afd

--- WebApplication_MVC_AAD_from_template/App_Start/Startup.Auth.cs
+++ WebApplication_MVC_AAD_from_template/App_Start/Startup.Auth.cs
@@ -12,6 +12,7 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Owin.Security.Notifications;
 using Microsoft.Owin.Infrastructure;
+using Microsoft.Owin;
 
 namespace WebApplication_MVC_AAD_from_template
 {
@@ -23,15 +24,44 @@ namespace WebApplication_MVC_AAD_from_template
         private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
         private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
         private static string authority = aadInstance + tenantId;
+       
         
 
 
         public void ConfigureAuth(IAppBuilder app)
         {
+
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
             /*{CookieDomain = "https://webapplicationmvcaadfed.azurewebsites.net" });
-            new OpenIdConnectProtocolValidator { RequireNonce = false };*/
+            new OpenIdConnectProtocolValidator { RequireNonce = false };*/                      
+
+            
+            app.UseOpenIdConnectAuthentication(
+               new OpenIdConnectAuthenticationOptions
+               {
+                    //See https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-aspnet-webapp#owin-startup-class
+
+                    // Sets the ClientId, authority, RedirectUri as obtained from web.config
+                   ClientId = clientId,
+                   Authority = authority,
+                    // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+                   PostLogoutRedirectUri = postLogoutRedirectUri,
+                   //redir,
+                   RedirectUri = redirectUri,                   
+                   Scope = OpenIdConnectScope.OpenIdProfile,
+                    // ResponseType is set to request the id_token - which contains basic information about the signed-in user
+                    ResponseType = OpenIdConnectResponseType.IdToken,
+                    // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                    // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                    // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter 
+                    TokenValidationParameters = new TokenValidationParameters()
+                   {
+                       ValidateIssuer = false
+                   }
+                   
+
+               });
 
             app.Use((context, next) =>
             {
@@ -45,38 +75,8 @@ namespace WebApplication_MVC_AAD_from_template
                 {
                     context.Request.Host = new Microsoft.Owin.HostString(host[0]);
                 }
-
                 return next();
             });
-
-
-
-            app.UseOpenIdConnectAuthentication(
-               new OpenIdConnectAuthenticationOptions
-               {
-                    //See https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-aspnet-webapp#owin-startup-class
-
-                    // Sets the ClientId, authority, RedirectUri as obtained from web.config
-                    ClientId = clientId,
-                   Authority = authority,
-                    // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
-                   PostLogoutRedirectUri = postLogoutRedirectUri,
-                   RedirectUri = redirectUri,
-                   Scope = OpenIdConnectScope.OpenIdProfile,
-                    // ResponseType is set to request the id_token - which contains basic information about the signed-in user
-                    ResponseType = OpenIdConnectResponseType.IdToken,
-                    // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
-                    // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
-                    // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter 
-                    TokenValidationParameters = new TokenValidationParameters()
-                   {
-                       ValidateIssuer = false
-                   }
-                   
-
-               });      
-            
-
 
         }
 
